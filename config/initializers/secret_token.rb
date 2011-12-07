d17b44ba1f1021c773b26e6c3ad1654288ed024c
David <daviddotterweich@gmail.com>
2011-12-06 20:11:59 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swaggdoctor::Application.config.secret_token = '301f0e42f9479e60bfdd725ca5b13796fe5963fc2e9233e9fb202c0ad10e9efa9d714c35e4bec7ac460f158f030455603e49393ff4b2dea4d31d8a468a4f582d'
